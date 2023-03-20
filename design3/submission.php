d84955717449195cf786f4e487bc3a4043ca0378
mountainvan <zengzhuo@msu.edu>
2023-03-20 11:54:29 -0400

    updated style of chat boxes

--- design3/submission.php
+++ design3/submission.php
@@ -1,7 +1,9 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-<link href="../cl/course.css" type="text/css" rel="stylesheet" />
+    <link href="../cl/course.css" type="text/css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!--<meta http-equiv="refresh" content="5">-->
 </head>
 <body>
 <!--chat box style-->
@@ -21,14 +23,14 @@
         color: green;
     }
     #con{
+        padding: 5px;
+        font-size: 12px;
         width: 300px;
         border: solid 1px;
         border-radius: 10px;
-        padding: 5px;
-        font-size: 12px;
-
     }
 </style>
+
 <div class="full">
 
 <p>When you are done, use this page to submit your diagram to the peer review system. </p>
@@ -46,68 +48,55 @@
 <p>You can submit as often as you like. You are encouraged to resubmit revisions after reading the peer reviews. You have 24 hours after any review to resubmit, even after the due date.</p>
 
 <?php echo Toggle::end(); ?>
+
+
 <?php echo $view->present_submissions(); ?>
-
-
-<!--week10 Zhuofan Zeng new added content-->
-<h3 style="text-align: center;background: #00723f;color: white;">Reviews of this assignment appear here.</h3>
-
-    <div id="">
-        <?php $res = $view->getReviewerContent(); ?>
-        <?php if (empty($res)) {?>
-        <?php }else{?>
-            <!-- foreach loop-->
-            <?php foreach ($res['allData'] as $key => $value){?>
-                <!--box for display comment-->
-                <div style="width: 769px; height: 200px; border: solid 1px; overflow-x: scroll;">
-                    <?php foreach ($value as $v){?>
-                        <?php $data = json_decode($v['metadata'],true);?>
-                        <?php if ($res['meid']==$v['reviewerid']){?>
-                            <?php echo "<div id='chat'>
-                                <ul>
-                                    <li id='name'>you</li>
-                                    <li id='con'>".$data['review']['review']."</li>
-                                    <li id='time'>".$v['time']."</li>
-                                </ul>
-                             </div>";
-                        }else{?>
-                            <?php echo "<div id='chat2'>
-                                <ul>
-                                    <li id='name'>$key</li>
-                                    <li id='con' style='background-color: #0c5645; color: white;'>".$data['review']['review']."</li>
-                                    <li id='time'>".$v['time']."</li>
-                                </ul>
-                            </div>"; }?>
-                    <?php }?>
-                </div>
-                <br>
-            <?php }?>
-            <br>
+<h3 style="text-align: center;background: #00723f;color: white;">
+    Reviews of this assignment appear here.
+</h3>
+<div id="">
+    <?php $res = $view->getReviewerContent(); ?>
+    <?php if (empty($res)) {?>
+    <?php }else{?>
+        <!-- foreach loop-->
+        <?php foreach ($res['allData'] as $key => $value){?>
+            <!--box for display comment-->
+            <div style="width: 778px; height: 300px; border: solid 1px #ccc;border-bottom: none;overflow-x: hidden; overflow-y: auto;">
+                <?php foreach ($value as $v){?>
+                    <?php $data = json_decode($v['metadata'],true);?>
+                    <?php if ($res['meid']==$v['reviewerid']){?>
+                        <?php echo "<div id='chat'>
+                            <ul>
+                                <!--<li id='name'>you</li>-->
+                                <li id='con'>".$data['review']['review']."</li>
+                                <li id='time'>".$v['time']."</li>
+                            </ul>
+                         </div>";
+                    }else{?>
+                        <?php echo "<div id='chat2'>
+                            <ul>
+                                <!--<li id='name'>$key</li>-->
+                                <li id='con' style='background-color: #0c5645; color: white;'>".$data['review']['review']."</li>
+                                <li id='time'>".$v['time']."</li>
+                            </ul>
+                        </div>"; }?>
+                <?php }?>
+            </div>
             <form action="/cl/api/review/saveContent" method="POST">
-                <!--box for intput comment-->
-                <textarea name="content" rows="5" cols="90"></textarea>
-                <!--drop down box-->
-                <select name="memberid" id="">
-                    <option value="">Please choose</option>
-                    <!--reviewData-->
-                    <?php foreach ($res['reviewData'] as $key => $value){?>
-                        <option value="<?php echo $value;?>">
-                            review<?php echo $key;?>
-                        </option>   <!--return key 0 or 1-->
-                    <?php }?>
-                </select>
+                <!--textarea for intput comment-->
+                <textarea style="border: 1px solid #ccc;" name="content" rows=1 cols="83"></textarea>
+                <input type="hidden" name="memberid" value="<?php echo $res['reviewData'][substr($key,-1)]; ?>">
                 <!--send button-->
-                <input type="submit" value="submit">
+                <input type="submit" value="submit" style="position:absolute;height: 26px;">
             </form>
+            <br/>
         <?php }?>
-    </div>
-    <!--week10 end-->
+    <?php }?>
+</div>
 <?php echo Toggle::begin("I get 'Not a valid image'", "p"); ?>
-
-<p>Submissions must be a PNG file. If you try to submit the Visual Paradigm .vpp file, it will fail 
-with this message. Re-read the instructions above 
+<p>Submissions must be a PNG file. If you try to submit the Visual Paradigm .vpp file, it will fail
+with this message. Re-read the instructions above
 on how to export an image of your design and follow them carefully.</p>
-
 <?php echo Toggle::end(); ?>
 </div>
 
